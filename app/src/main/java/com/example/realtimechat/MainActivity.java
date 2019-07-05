ec57f35c74b0d8ec75b645b1da0d3c9ab62c81fc
Cleyson Diego da Silva <cleyson.dasilva@gastecnlogia.com.br>
2019-07-05 15:29:01 -0300

    Atualizando dependencias e compile sdk version

--- app/src/main/java/com/example/realtimechat/MainActivity.java
+++ app/src/main/java/com/example/realtimechat/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.realtimechat;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
@@ -57,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
                     String value = ds.child("text").getValue(String.class);
                     String email = ds.child("user").getValue(String.class);
                     System.out.println("Value is: " + value);
-                    txtMessages.addView();
                 }
             }
             @Override
